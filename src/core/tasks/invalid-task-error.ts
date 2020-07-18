cb905907bbaafb987a8a075e178df89c9eebfeb1
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-07-18 14:39:11 +0700

    Better error message tests

--- src/core/tasks/invalid-task-error.ts
+++ src/core/tasks/invalid-task-error.ts
@@ -1,5 +1,20 @@
+/**
+ * @packageDocumentation
+ * @module run-z
+ */
+
+/**
+ * An error thrown when task specifier syntax is invalid.
+ */
 export class InvalidZTaskError extends Error {
 
+  /**
+   * Constructs invalid task error.
+   *
+   * @param message  Error message.
+   * @param commandLine  Command line containing invalid specifier.
+   * @param position  Error position in command line.
+   */
   constructor(
       message: string,
       readonly commandLine: string,
