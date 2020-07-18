81c502d65235cf89abca0488718f7d8136168a03
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-07-18 14:05:50 +0700

    Implement undefined task execution

--- src/core/tasks/impl/unknown.task.spec.ts
+++ src/core/tasks/impl/unknown.task.spec.ts
@@ -1,4 +1,6 @@
+import { asis, valueProvider } from '@proc7ts/primitives';
 import { TestPlan } from '../../../spec';
+import { UnknownZTaskError } from '../unknown-task-error';
 import { UnknownZTask } from './unknown.task';
 
 describe('UnknownZTask', () => {
@@ -14,5 +16,30 @@ describe('UnknownZTask', () => {
     const call = await testPlan.plan('absent');
 
     expect(call.task).toBeInstanceOf(UnknownZTask);
+  });
+
+  describe('exec', () => {
+    it('throws when absent', async () => {
+
+      const call = await testPlan.plan('absent');
+
+      expect(await call.exec().whenDone().catch(asis)).toBeInstanceOf(UnknownZTaskError);
+    });
+    it('does not throw when called with `if-present` attribute', async () => {
+
+      const call = await testPlan.plan(
+          'absent',
+          {
+            async plan(planner) {
+              await planner.call({
+                task: planner.plannedCall.task,
+                params: valueProvider({ attrs: { 'if-present': ['on'] } }),
+              });
+            },
+          },
+      );
+
+      expect(await call.exec().whenDone()).toBeUndefined();
+    });
   });
 });
--- src/core/tasks/impl/unknown.task.ts
+++ src/core/tasks/impl/unknown.task.ts
@@ -1,5 +1,7 @@
 import type { ZPackage } from '../../packages';
+import type { ZTaskExecution } from '../../plan/task-execution';
 import { ZTaskSpec } from '../task-spec';
+import { UnknownZTaskError } from '../unknown-task-error';
 import { AbstractZTask } from './abstract.task';
 
 /**
@@ -7,8 +9,14 @@ import { AbstractZTask } from './abstract.task';
  */
 export class UnknownZTask extends AbstractZTask<ZTaskSpec.Unknown> {
 
-    constructor(target: ZPackage, name: string) {
-        super(target, name, ZTaskSpec.unknown);
+  constructor(target: ZPackage, name: string) {
+    super(target, name, ZTaskSpec.unknown);
+  }
+
+  exec(execution: ZTaskExecution<ZTaskSpec.Unknown>): void | PromiseLike<unknown> {
+    if (!execution.call.params().flag('if-present')) {
+      return Promise.reject(new UnknownZTaskError(this.target.name, this.name));
     }
+  }
 
 }
