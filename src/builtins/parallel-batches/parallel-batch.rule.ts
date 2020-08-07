8f4424948a56c78d65a8695ce8096884ca03754b
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-08-07 09:20:50 +0700

    Declare `ZParallelBatch` as interface and constant

--- src/builtins/parallel-batches/parallel-batch.rule.ts
+++ src/builtins/parallel-batches/parallel-batch.rule.ts
@@ -5,23 +5,45 @@
 import type { ZBatching, ZBatchRule } from '../../core';
 
 /**
- * Controller of parallel execution of batched rules.
+ * Controller of parallel execution of batched tasks.
  *
  * This class is a batch processing rule.
  */
-export class ZParallelBatch {
+export interface ZParallelBatch {
+
+  /**
+   * Whether batched rules will be executed parallel to each other.
+   */
+  readonly isParallel: boolean;
+
+  /**
+   * Makes batched rules execute in parallel to each other.
+   *
+   * @param parallel  `true` or nothing to execute batched rules in parallel to each other, or `false` to execute them
+   * sequentially.
+   *
+   * @returns Updated batching policy.
+   */
+  makeParallel(parallel?: boolean): ZBatching;
+
+}
+
+/**
+ * @internal
+ */
+class ZParallelBatch$ implements ZParallelBatch {
 
   static newInstance(
       context: ZBatchRule.Context<ZParallelBatch>,
       parallel = false,
   ): ZBatchRule.Instance<ZParallelBatch> {
 
-    const control = new ZParallelBatch(context, parallel);
+    const control = new ZParallelBatch$(context, parallel);
 
     return {
       control,
       moveTo(context, transiently) {
-        return !transiently && ZParallelBatch.newInstance(context, control.isParallel);
+        return !transiently && ZParallelBatch$.newInstance(context, control.isParallel);
       },
       processBatch({ dependent, batched }) {
         dependent.makeParallel(Array.from(batched, call => call.task));
@@ -50,8 +72,13 @@ export class ZParallelBatch {
    */
   makeParallel(parallel = true): ZBatching {
     return this._context.updateInstance(
-        context => parallel ? ZParallelBatch.newInstance(context, parallel) : undefined,
+        context => parallel ? ZParallelBatch$.newInstance(context, parallel) : undefined,
     );
   }
 
 }
+
+/**
+ * Parallel batched tasks execution rule.
+ */
+export const ZParallelBatch: ZBatchRule<ZParallelBatch> = ZParallelBatch$;
