55be55b702300598b63314d89db211c79d11f4d9
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-08-08 15:49:53 +0700

    Clearer test expectations

--- src/cli/impl/format-option-error.spec.ts
+++ src/cli/impl/format-option-error.spec.ts
@@ -4,34 +4,32 @@ import { formatZOptionError } from './format-option-error';
 describe('formatZOptionError', () => {
 
   let args: readonly string[];
-  let commandLine: string;
   let message: string;
 
   beforeEach(() => {
     args = ['node', 'yarn', 'run-z', 'test', '--option'];
-    commandLine = args.slice(2).join(' ');
     message = 'Error message';
   });
 
   it('underlines invalid option', () => {
     expect(format({ args, index: 3 })).toEqual([
-        message,
-        commandLine,
-        '_'.repeat(6) + '^'.repeat(4),
+      message,
+      'run-z test --option',
+      '______^^^^',
     ]);
   });
   it('underlines part of invalid option', () => {
-    expect(format({ args, index: 4, offset: 2 })).toEqual([
+    expect(format({ args, index: 4, offset: 2, endOffset: 5 })).toEqual([
       message,
-      commandLine,
-      '_'.repeat(13) + '^'.repeat(6),
+      'run-z test --option',
+      '_____________^^^',
     ]);
   });
   it('underlines several options', () => {
     expect(format({ args, index: 3, endIndex: 5, offset: 2, endOffset: 2 })).toEqual([
       message,
-      commandLine,
-      '_'.repeat(8) + '^'.repeat(5),
+      'run-z test --option',
+      '________^^^^^',
     ]);
   });
 
