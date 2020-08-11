f8c94b7a4f3e0fc1a93f08aecf63ba4ca8857157
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-08-11 14:19:02 +0700

    Add terminal colors control options

---
+++ src/builtins/colors.builtin.ts
@@ -0,0 +1,15 @@
+/**
+ * @packageDocumentation
+ * @module run-z/builtins
+ */
+import { chalkZColorOptions } from '@run-z/optionz/colors';
+import type { ZExtension } from '../core';
+
+/**
+ * Builtin extension to control terminal color support.
+ *
+ * Supports `--color`, `--no-color`, and `--no-colors` options.
+ */
+export const ZColorsBuiltin: ZExtension = {
+  options: chalkZColorOptions(),
+};
