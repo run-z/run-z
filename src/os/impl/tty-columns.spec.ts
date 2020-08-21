d67c649e787da9c43d2ecd8380e877694d2832ca
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-08-21 12:03:29 +0700

    Test `ttyColumns()` utility

---
+++ src/os/impl/tty-columns.spec.ts
@@ -0,0 +1,35 @@
+import { ttyColumns } from './tty-columns';
+
+describe('ttyColumns', () => {
+
+  let prevColumns: number;
+  let prevEnv: string | undefined;
+
+  beforeEach(() => {
+    prevColumns = process.stdout.columns;
+    prevEnv = process.env.COLUMNS;
+  });
+  afterEach(() => {
+    process.stdout.columns = prevColumns;
+    if (prevEnv != null) {
+      process.env.COLUMNS = prevEnv;
+    } else {
+      delete process.env.COLUMNS;
+    }
+  });
+
+  it('detects the number of columns in stdout', () => {
+    process.stdout.columns = 123;
+    expect(ttyColumns()).toBe(123);
+  });
+  it('detects the number of columns by COLUMNS environment variable', () => {
+    process.stdout.columns = 0;
+    process.env.COLUMNS = '213';
+    expect(ttyColumns()).toBe(213);
+  });
+  it('falls back to 80', () => {
+    process.stdout.columns = 0;
+    delete process.env.COLUMNS;
+    expect(ttyColumns()).toBe(80);
+  });
+});
