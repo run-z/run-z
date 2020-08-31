dd5b504785b14aa12acc03830fbda83b3e0f52d4
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-08-31 15:21:59 +0700

    Add execution options to `runZ()`.

--- src/cli/run-z.cli.ts
+++ src/cli/run-z.cli.ts
@@ -5,29 +5,95 @@
 import { AbortedZExecutionError, FailedZExecutionError } from '@run-z/exec-z';
 import { ZOptionError } from '@run-z/optionz';
 import { StandardZSetup } from '../builtins';
-import { UnknownZTaskError } from '../core';
+import { UnknownZTaskError, ZPackageLocation, ZSetup, ZShell } from '../core';
 import { SystemZShell, ZPackageDirectory } from '../os';
 import { formatZOptionError } from './impl';
 
-export function runZ(): Promise<void> {
-  return doRunZ().catch(logZError);
+/**
+ * `run-z` execution options.
+ */
+export interface RunZOpts {
+
+  /**
+   * Known task name.
+   *
+   * Used to reconstruct original command line when issued via `yarn run` or `npm run`.
+   *
+   * @default `process.env.npm_lifecycle_event`.
+   *
+   * @see {@link ZTaskBuilder.applyArgv}.
+   */
+  readonly taskName?: string;
+
+  /**
+   * An index of command line argument to start processing from.
+   *
+   * @default `2`.
+   */
+  readonly fromIndex?: number;
+
+  /**
+   * Working directory location.
+   *
+   * @default {@link ZPackageDirectory.open() current directory}.
+   */
+  readonly location?: ZPackageLocation;
+
+  /**
+   * Task execution setup.
+   *
+   * @default New {@link StandardZSetup} instance.
+   */
+  readonly setup?: ZSetup;
+
+  /**
+   * Task execution shell to use.
+   *
+   * @default New {@link SystemZShell} instance.
+   */
+  readonly shell?: ZShell;
+
+}
+
+/**
+ * Executes tasks.
+ *
+ * Logs execution errors.
+ *
+ * @param args  Command line arguments. `process.argv` by default.
+ * @param opts  Execution options.
+ *
+ * @returns A promise resolved when execution succeeds or rejected when it is failed.
+ */
+export function runZ(
+    args: readonly string[] = process.argv,
+    opts: RunZOpts = {},
+): Promise<void> {
+  return doRunZ(args, opts).catch(logZError);
 }
 
 /**
  * @internal
  */
-async function doRunZ(): Promise<void> {
+async function doRunZ(
+    args: readonly string[],
+    {
+      taskName = process.env.npm_lifecycle_event,
+      fromIndex,
+      location = ZPackageDirectory.open(),
+      setup = new StandardZSetup(),
+      shell = new SystemZShell(setup),
+    }: RunZOpts,
+): Promise<void> {
 
-  const setup = new StandardZSetup();
-  const shell = new SystemZShell(setup);
-  const currentDir = ZPackageDirectory.open();
-  const target = await setup.packageResolver.get(currentDir);
+  const target = await setup.packageResolver.get(location);
   const builder = await setup.taskFactory
       .newTask(target, 'run-z')
       .applyArgv(
-          process.env.npm_lifecycle_event,
-          process.argv,
+          taskName,
+          args,
           {
+            fromIndex,
             options: shell.options(),
           },
       );
