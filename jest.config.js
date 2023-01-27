a9e98158b00207aa8f046ac12a9566f852a351f9
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-01-27 17:47:38 +0700

    Remove redundant module mappings

--- jest.config.js
+++ jest.config.js
@@ -21,12 +21,6 @@ export default {
     },
   },
   extensionsToTreatAsEsm: ['.ts'],
-  moduleNameMapper: {
-    // Subpath imports not supported by Jest
-    // See https://github.com/facebook/jest/issues/11100
-    '^#ansi-styles$': 'ansi-styles',
-    '^#supports-color$': 'supports-color',
-  },
   reporters: [
     'default',
     [
